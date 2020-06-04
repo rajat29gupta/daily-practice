3774c0da9a82f1c20434cd08ff0bac825119cb9d
Rajat <rajat.gupta@i2k2.com>
2020-06-04 22:15:40 +0530

    Added 04-06 Update

---
+++ june.js
@@ -0,0 +1,59 @@
+/********
+ *
+ * 04 June *
+ *
+ ********/
+
+// Write a JavaScript program to remove a character at the specified position of a given string and return the new string.
+// function foo(stringName, position) {
+//   let partA = stringName.substring(0, position);
+//   let partB = stringName.substring(position + 1, stringName.length);
+//   return partA + partB;
+// }
+
+// const res =foo("rajat",2);
+// console.log(res);
+
+/****************************************************************   ENDS    ***********************************************************************************************/
+// Write a JavaScript program to create a new string from a given string changing the position of first and last characters. The string length must be greater than or equal to 1.
+// function poo(charInput) {
+//   if (charInput.length <= 1) {
+//     return charInput;
+//   }
+//   let midChar = charInput.substring(1, charInput.length - 1);
+//   let revChar = charInput.charAt(charInput.length - 1) + midChar + charInput[0];
+//   return revChar;
+// }
+
+// const res = poo("rajat");
+
+// console.log(res);
+
+/****************************************************************   ENDS    ***********************************************************************************************/
+// Write a JavaScript program to check whether a given positive number is a multiple of 3 or a multiple of 7
+// function foo(str) {
+//   if (str < 0) {
+//     return false;
+//   }
+//   if (str % 3 == 0 || str % 7 == 0) {
+//     return str % 3 == 0 || str % 7 == 0;
+//   }else{
+//       return false
+//   }
+// //   if (str % 3 == 0) {
+// //     return 2;
+// //   }
+// //   if (str % 7 == 0) {
+// //     return 1;
+// //   }
+// }
+// console.log(foo(4));
+
+/****************************************************************   ENDS    ***********************************************************************************************/
+/********
+ *
+ * 05 June *
+ *
+ ********/
+
+ // Write a JavaScript program to create a new string from a given string taking the last 3 characters and added at both the front and back. The string length must be 3 or more.
