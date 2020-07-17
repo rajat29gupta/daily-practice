28f6df5e629516d4ebe82f93563182ebc44ec5c3
rajat <rajat.gupta@i2k2.com>
2020-07-17 15:49:08 +0530

    added 17-04 Update

--- july.js
+++ july.js
@@ -25,3 +25,38 @@
 // console.log(
 //   bifurcate(["beep", "boop", "foo", "bar"], [true, true, false, true])
 // );
+
+// Write a JavaScript program to get an object containing the parameters of the current URL.
+
+// const getUrl = (url) => {
+//   let arr = url.includes("?")
+//     ? url
+//         .slice(url.indexOf("?") + 1)
+//         .split("&")
+//         .map((s) => s.split("="))
+//     : [];
+//   return arr;
+// //   return Object.fromEntries(arr); // Not Defined in node
+// };
+
+// console.log(getUrl("http://url.com/page?name=Adam&surname=Smith")); // {name: "Adam", surname: "Smith"}
+// console.log(getUrl("google.com")); // {}
+
+// Write a JavaScript program to Initialize a two dimension array of given width and height and value.
+// const initial2DArray = (w, h, val) =>
+//   new Array(h).fill(h)
+
+// console.log(initial2DArray(2, 2, 0)); // [[0,0],[0,0]]
+// console.log(initial2DArray(3, 3, 3)); // [[3,3,3],[3,3,3],[3,3,3]]
+
+// Write a JavaScript program to check whether all elements in a given array are equal or not.
+
+// const foo = (arr) => arr.every((val) => val === arr[0]);
+// console.log(foo([1, 2, 3, 4, 5, 6]));
+// console.log(foo([12, 12, 12, 12]));
+
+// Write a JavaScript program to cast the provided value as an array if it's not one.
+// const foo = (val) => (Array.isArray(val) ? val : [val]);
+
+// console.log(foo("w3r"));
+// console.log(foo([100]));
