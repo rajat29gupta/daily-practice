035d7c030d3f8f2332767e316efb5faea5e289a9
rajat <rajat.gupta@i2k2.com>
2020-08-05 16:48:45 +0530

    Added 05-08 Update

---
+++ august.js
@@ -0,0 +1,24 @@
+// Write a JavaScript function to check whether an `input` is a string or not
+// const foo = (str) => typeof str === "string";
+// console.log(foo('rajat'));
+// console.log(foo([1, 2, 4, 0]));
+
+// Write a JavaScript function to check whether a string is blank or not.
+// const foo = (str) => (str.length > 0 ? "Not Bank" : "Blank");
+// console.log(foo(""));
+// console.log(foo("abc"));
+
+// Write a JavaScript function to split a string and convert it into an array of words.
+// const foo = (str) => str.split(" ");
+// console.log(foo("Robin Singh"));
+
+// Write a JavaScript function to extract a specified number of characters from a string.
+// const foo = (str, num) => str.slice(0, num);
+// console.log(foo("Robin Singh", 4));
+
+// Write a JavaScript function to convert a string in abbreviated form.
+// const foo = (str) => {
+//   let abb = str.split(" ");
+//   return `${abb[0]} ${abb[1].charAt(0)}`;
+// };
+// console.log(foo("Robin Singh"));
